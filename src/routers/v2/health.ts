c28d59cb7d28a462f79fc52cc1c2194fc6d10bc5
Lukasz Ch <l.chmielewski@stuart.com>
2024-11-18 18:08:41 +0100

    added error catching

--- src/routers/v2/health.ts
+++ src/routers/v2/health.ts
@@ -1,11 +1,15 @@
-import express, { Request, Response, Router } from 'express';
+import express, { NextFunction, Request, Response, Router } from 'express';
 import { Routes, Version } from '../routes';
 
 const router: Router = express.Router();
 
-router.get(Routes.root, (req: Request, res: Response) => {
-  console.log(`${req.baseUrl}`);
-  res.send(`${Version.v2.toUpperCase()}: Service is up and running`);
+router.get(Routes.root, (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log(`${req.baseUrl}`);
+    res.send(`${Version.v2.toUpperCase()}: Service is up and running`);
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
